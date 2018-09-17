ec248b60815ba0d7e291960b57533f1ccedd7005
martindale.40@osu.edu <evanmartindale@gmail.com>
2018-09-17 16:57:55 -0400

    Determine cwd, inport excel file and save as panda df

--- analysis.py
+++ analysis.py
@@ -13,23 +13,36 @@ import pandas as pd
 import glob
 import os
 import sys
-#print (sys.argv)
+
+
 
 #Start off by assuming we are working in Windows
+##############################################################################
+#GET WORKING DIRECTORY
+##############################################################################
+for n,x in enumerate(os.listdir()):
+    print("["+str(n)+"]: "+x)
+    
 #folder = input("Please enter the directory that you would like to work in:")
-folder = "C:/Users/martindale.40/Box Sync/Palmer Lab Research-Martindale/Hemoglobin_Purification/hHb Purification/TFF-hHb-48";
+#folder = "C:/Users/martindale.40/Box Sync/Palmer Lab Research-Martindale/Hemoglobin_Purification/hHb Purification/TFF-hHb-48";
 #print(folder);
-os.chdir(folder);
+#os.chdir(folder);
 #print(os.listdir());
-file = "TFF-hHb-48_Batch_Data.xlsx";
+path = os.path.dirname(os.path.realpath(__file__));
+path = path+"/test/";
+##############################################################################
+
+##############################################################################
+#READ IN DATA FROM EXCEL FILE AND SAVE AS DATAFRAME OBJECTS
+##############################################################################
+file = path+"TFF-hHb-48_Batch_Data.xlsx";
 excel = pd.ExcelFile(file);
 
-Input = pd.read_excel(excel, "Input");
-washLysis = pd.read_excel(excel, "WashLysis");
-TFF = pd.read_excel(excel, "TFF");
-spectra = pd.read_excel(excel, "Spectra");
+sub = path+"/dataframes"
+if not os.path.isdir(sub):
+    os.makedirs(sub)
 
-print(Input.head())
-print(washLysis.head())
-print(TFF.head())
-print(spectra.head(1))
+for x in excel.sheet_names:
+        df = pd.read_excel(excel,x);
+        df.to_pickle(sub+"/"+x+".pk1")
+##############################################################################
